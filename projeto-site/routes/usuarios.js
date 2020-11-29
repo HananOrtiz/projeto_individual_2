3c7edb58baa3643c7d60a80e87d52476b3ee6eb7
HananOrtiz <hanan.spinola@bandtec.com.br>
2020-11-29 19:09:50 -0300

    Aula_for_while

--- projeto-site/routes/usuarios.js
+++ projeto-site/routes/usuarios.js
@@ -9,10 +9,10 @@ let sessoes = [];
 router.post('/autenticar', function(req, res, next) {
 	console.log('Recuperando usuário por login e senha');
 
-	var login = req.body.nickname; // depois de .body, use o nome (name) do campo em seu formulário de login
+	var nickname = req.body.nickname; // depois de .body, use o nome (name) do campo em seu formulário de login
 	var senha = req.body.senha; // depois de .body, use o nome (name) do campo em seu formulário de login	
 	
-	let instrucaoSql = `select * from usuario where login='${login}' and senha='${senha}'`;
+	let instrucaoSql = `select * from usuario where nickname='${nickname}' and senha='${senha}'`;
 	console.log(instrucaoSql);
 
 	sequelize.query(instrucaoSql, {
