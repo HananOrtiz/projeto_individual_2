ef396e72907f5f94ee0aec1b75aa1383e3339898
HananOrtiz <hanan.spinola@bandtec.com.br>
2020-11-29 12:50:07 -0300

    atualizacao

--- projeto-site/routes/usuarios.js
+++ projeto-site/routes/usuarios.js
@@ -9,7 +9,7 @@ let sessoes = [];
 router.post('/autenticar', function(req, res, next) {
 	console.log('Recuperando usuário por login e senha');
 
-	var login = req.body.login; // depois de .body, use o nome (name) do campo em seu formulário de login
+	var login = req.body.nickname; // depois de .body, use o nome (name) do campo em seu formulário de login
 	var senha = req.body.senha; // depois de .body, use o nome (name) do campo em seu formulário de login	
 	
 	let instrucaoSql = `select * from usuario where login='${login}' and senha='${senha}'`;
